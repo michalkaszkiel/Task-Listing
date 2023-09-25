a0e987f39bb0f279d38a0d34f32312103ad3ccd3
Michal <120386627+michalkaszkiel@users.noreply.github.com>
2023-09-25 18:00:28 +0200

    III

--- task-list/src/components/LandingPage.jsx
+++ task-list/src/components/LandingPage.jsx
@@ -1,22 +1,26 @@
 import list from "../images/list.png";
 import { Fade } from "react-reveal";
+import { Link } from "react-router-dom";
+
 export const LandingPage = () => {
     const listed = list;
     return (
         <>
             <div className="Landing-Page-Main">
                 <Fade duration={2500} delay={500} timingFunction="ease-in">
-                    <div className="Landing-text">
+                    <div className="Landing-text ">
                         <div className="Landing-Page-Sub-Text">
                             <h1>Keep your tasks in one place...</h1>
                             <p>And never forget a thing.</p>
                         </div>
                         <div className="Landing-Page-Image-Cont">
-                            <img
-                                alt="list"
-                                src={listed}
-                                className="Landing-Page-Image"
-                            ></img>
+                            <Link to="/list">
+                                <img
+                                    alt="list"
+                                    src={listed}
+                                    className="Landing-Page-Image pulse-on"
+                                ></img>
+                            </Link>
                         </div>
                     </div>
                 </Fade>
---
+++ task-list/src/components/List.jsx
@@ -0,0 +1,7 @@
+export const List = () => {
+    return (
+        <div className="List-Main">
+            <div></div>
+        </div>
+    );
+};
