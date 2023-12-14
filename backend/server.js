f6ada7d85ffd1890c3953173d0abacd91ffc325e
Michal <149673103+michalkaszkiel11@users.noreply.github.com>
2023-12-14 19:13:17 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,26 @@ import cors from "cors";
 import mongoose from "mongoose";
 import router from "./routes/router.js";
 import cookieParser from "cookie-parser";
-import { __dirname } from "./global.js"; // Import __dirname from global.js
-import path from "path"; // Import path module
 
 const app = express();
 dotenv.config();
 app.use(cookieParser());
-const port = process.env.PORT || 10000;
+app.use(express.json());
+app.use(cors(corsOptions));
+app.listen(port, () => {
+    console.log(`The server is listening 🐒 on port ${port}`);
+});
+app.get("/check", (req, res) => {
+    res.send("Server is awake!");
+});
 
+const port = process.env.PORT || 10000;
+const corsOptions = {
+    origin: "*", //allow all origins * = wildcard
+    methods: ["HEAD", "GET", "POST", "PATCH", "DELETE"],
+    credentials: true, //allow cookies to be sent with requests
+};
+app.use("/api/task-list", router);
 mongoose
     .connect(
         `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`,
@@ -22,35 +34,11 @@ mongoose
     )
     .then(() => {
         console.log("Database connected! 😃");
+        app.listen(port, () => {
+            console.log(`🥦The server is listening on port ${port}🥦`);
+        });
     })
     .catch((error) => {
         console.log(error.message);
         console.log("🤨");
     });
-// Serve static files from the 'build' directory
-const corsOptions = {
-    origin: "*",
-    methods: ["HEAD", "GET", "POST", "PATCH", "DELETE"],
-    credentials: true, //allow cookies to be sent with requests
-};
-app.use(express.json());
-app.use(cors(corsOptions));
-app.use("/api/task-list", router);
-app.use(express.static(path.join(__dirname, "build")));
-
-// Handle all routes and serve the index.html file
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-// Handle invalid paths with a 500 status
-app.all("*", (req, res) => {
-    res.status(500).send("Invalid path");
-});
-
-app.listen(port, () => {
-    console.log(`The server is listening 🐒 on port ${port}`);
-});
-app.get("/check", (req, res) => {
-    res.send("Server is awake!");
-});
