ca3a0e69f57219233e999987ad691d0e1549c3b1
Michal <149673103+michalkaszkiel11@users.noreply.github.com>
2024-03-27 19:33:44 +0100

    hosting change

--- task-list/src/util/axiosInstance.js
+++ task-list/src/util/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const createApiInstance = (token) => {
     return axios.create({
-        baseURL: "https://task-list-vk02.onrender.com/api/task-list",
+        baseURL: "https://task-listing.onrender.com/api/task-list",
         headers: {
             Authorization: `Bearer ${token}`,
         },
